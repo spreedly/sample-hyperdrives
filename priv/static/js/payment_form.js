b57e14818bcc752d9b324bdc92f23337348bff14
Duff OMelia <duff@omelia.org>
2014-12-16 12:49:55 -0500

    Update to latest iFrame stuff

--- priv/static/js/payment_form.js
+++ priv/static/js/payment_form.js
@@ -1,15 +1,30 @@
-var spreedlyPaymentFrame = new Spreedly.PaymentFrame();
 
-spreedlyPaymentFrame.on('token', function(token) {
+var spreedlyPaymentFrame = new Spreedly.PaymentFrame("8rLAE4vizuIQyA0zorQHIMrluuM");
+
+function submitPaymentForm() {
+  var paymentMethodFields = ['first_name', 'last_name', 'month', 'year']
+  for(var i = 0; i < paymentMethodFields.length; i++) {
+    var field = paymentMethodFields[i];
+    spreedlyPaymentFrame.setParam(field, document.getElementById(field).value)
+  }
+  spreedlyPaymentFrame.submit();
+}
+
+spreedlyPaymentFrame.on('paymentMethod', function(token, pmData) {
   var tokenField = document.getElementById("payment_method_token");
   tokenField.setAttribute("value", token);
   var masterForm = document.getElementById('payment-form');
   masterForm.submit();
 });
 
-spreedlyPaymentFrame.on('error', function(message) {
-  console.log(message);
+spreedlyPaymentFrame.on('errors', function(errors) {
+  console.log(errors);
+  for(var i = 0; i < errors.length; i++) {
+    var error = errors[i];
+    if(error["attribute"]) {
+      spreedlyPaymentFrame.setStyle('input#spf-' + error["attribute"], "border: 1px solid red;");
+    }
+  }
 });
 
 spreedlyPaymentFrame.bind(document.getElementById('spreedly-iframe'));
-
