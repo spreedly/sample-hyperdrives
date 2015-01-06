94d0eb905600099d4a321df06731e735dca379c7
Duff OMelia <duff@omelia.org>
2015-01-06 15:52:24 -0500

    Improve styling a bit

--- priv/static/js/payment_form.js
+++ priv/static/js/payment_form.js
@@ -46,8 +46,10 @@ spreedlyPaymentFrame.on('ready', function() {
 spreedlyPaymentFrame.on('config', function(frame) {
   frame.setStyle('body', 'margin: 0px;');
   frame.setStyle('input', 'line-height: 20px;');
-  frame.setStyle('.spf-field.spf-number', 'float: left; display: inline-block; margin-right: 1%');
+  frame.setStyle('.spf-field.spf-number', 'float: left; display: inline-block; margin-right: 1%; width: 162px;');
   frame.setStyle('.spf-field.spf-verification_value', 'float: left; display: inline-block; margin-right: 1%;');
+  frame.setStyle('input#spf-number', 'width: 100%; font-size: 14px;');
+  frame.setStyle('input#spf-verification_value', 'width: 100%; font-size: 14px;');
   frame.setStyle('label', 'display: block; font-family:"Helvetica Neue",Helvetica,Arial,sans-serif; font-size: 14px; margin-bottom: 0.25em; margin-top: 0.8em;');
   frame.setText('label[for="spf-number"]', 'Card Number');
 });
